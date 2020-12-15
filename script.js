e0ad8896e7fb82cb5dc0ce366095aea9bbffa65f
Aida Yrysbekova <aidiko20@gmail.com>
2020-12-14 20:29:47 -0500

    added some logic

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ const editBtn = document.querySelector('.edit');
 const deleteBtn = document.querySelector('.delete');
 
 
-const mail = notesEl.querySelector('.main');
+const main = notesEl.querySelector('.main');
 const textArea = notesEl.querySelector('textarea');
 editBtn.addEventListener('click', () => {
     main.classList.toggle('hidden');
     textArea.classList.toggle('hidden');
+})
+
+textArea.addEventListener('input', (e) => {
+    const { value } = e.target;
+    
+    main.innerHTML = marked(value);
 })
