15e5d9c2d081dd4dca0ffa47998c8c93e84bb6bd
Aida Yrysbekova <aidiko20@gmail.com>
2020-12-14 20:57:24 -0500

    changed ui and added add note btn

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
-const notesEl = document.querySelector('.notes')
-const editBtn = document.querySelector('.edit');
-const deleteBtn = document.querySelector('.delete');
+const addBtn = document.getElementById('add');
 
+addBtn.addEventListener('click', () => {
+    addNewNote();
+});
 
-const main = notesEl.querySelector('.main');
-const textArea = notesEl.querySelector('textarea');
-editBtn.addEventListener('click', () => {
-    main.classList.toggle('hidden');
-    textArea.classList.toggle('hidden');
-})
+function addNewNote() {
+    const note = document.createElement('div');
+    note.classList.add('note');
 
-textArea.addEventListener('input', (e) => {
-    const { value } = e.target;
+    note.innerHTML = `
+        <div class="notes">
+        <div class="tools">
+            <button class="edit"><i class="fas fa-edit"></i></button>
+            <button class="delete"><i class="fas fa-trash-alt"></i></button>
+        </div>
+        <div class="main hidden"></div>
+        <textarea></textarea>
+    </div>
+    `;
+
+    const editBtn = note.querySelector('.edit');
+    const deleteBtn = note.querySelector('.delete');
+
+    const main = note.querySelector('.main');
+    const textArea = note.querySelector('textarea');
+
+    editBtn.addEventListener('click', () => {
+        main.classList.toggle('hidden');
+        textArea.classList.toggle('hidden');
+    })
+
+    deleteBtn.addEventListener('click', () => {
+        note.remove();
+    })
     
-    main.innerHTML = marked(value);
-})
+    textArea.addEventListener('input', (e) => {
+        const { value } = e.target;
+    
+        main.innerHTML = marked(value);
+    })
+    document.body.appendChild(note);
+}
